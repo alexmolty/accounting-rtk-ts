9092218e86194b8dbc4869f004ddce53bacc4a1b
alexmolt <alexmolt@mail.ru>
2025-08-27 19:34:43 +0300

    update user, change password complete

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,6 +1,7 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import type {UserRegister} from "../../utils/types";
+import type {UserData, UserRegister} from "../../utils/types";
 import {base_url, createToken} from "../../utils/constants.ts";
+import type {RootState} from "../../app/store.ts";
 
 export const registerUser = createAsyncThunk(
     `user/register`,
@@ -41,5 +42,47 @@ export const fetchUser = createAsyncThunk(
         }
         const data = await response.json();
         return {user: data, token};
+    }
+)
+
+export const updateUser = createAsyncThunk<UserData, UserData, { state: RootState }>(
+    "user/update",
+    async (user, {getState}) => {
+        const response = await fetch(`${base_url}/account/user/${getState().user.login}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: getState().token
+            },
+            body: JSON.stringify(user)
+        });
+        if (response.status === 401) {
+            throw new Error(`Invalid credentials`);
+        }
+        if (!response.ok) {
+            throw new Error(`Something went wrong`);
+        }
+        const {firstName, lastName} = await response.json();
+        return {firstName, lastName}
+    }
+);
+
+export const changePassword = createAsyncThunk<string, string, { state: RootState }>(
+    "user/password",
+    async (newPassword: string, {getState}) => {
+        const response = await fetch(`${base_url}/account/user/password`, {
+            method: "Patch",
+            headers: {
+                Authorization: getState().token,
+                "X-Password": newPassword
+            }
+        });
+        if (response.status === 401) {
+            throw new Error(`Invalid credentials`);
+        }
+        if (!response.ok) {
+            throw new Error(`Something went wrong`);
+        }
+        return createToken(getState().user.login, newPassword)
     }
 )
